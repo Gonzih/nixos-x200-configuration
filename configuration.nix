b9abe97168bec772428c9c2867b30b5f873515c2
Max Gonzih <gonzih@gmail.com>
2014-11-14 18:47:50 +0100

    Commit configuration Fri Nov 14 18:47:50 CET 2014

--- configuration.nix
+++ configuration.nix
@@ -51,6 +51,50 @@
   # List packages installed in system profile. To search by name, run:
   # -env -qaP | grep wget
   environment = {
+    etc = {
+      "X11/xorg.conf.d/10-evdev.conf".text = ''
+        Section "InputClass"
+                Identifier  "Marble Mouse"
+                MatchProduct "Logitech USB Trackball"
+                MatchIsPointer "on"
+                MatchDevicePath "/dev/input/event*"
+                Driver "evdev"
+        #       Physical button #s:     A b D - - - - B C    b = A & D simultaneously;   - = no button
+        #       Option "ButtonMapping" "1 8 3 4 5 6 7 2 2"
+                Option "ButtonMapping" "1 2 3 4 5 6 7 2 8"   #  From my configuration
+        #       Option "ButtonMapping" "1 8 3 4 5 6 7 2 2"   #  For right-hand placement
+        #       Option "ButtonMapping" "3 8 1 4 5 6 7 2 2"   #  For left-hand placement
+        #
+        #       EmulateWheel refers to emulating a mouse wheel using Marble Mouse trackball.
+                Option "GrabDevice" "true"
+                Option "EmulateWheel" "true"
+                Option "EmulateWheelButton" "8"              # Factory default; use "9" for left-side placement.
+                Option "EmulateWheelInertia" "30"            # Factory default: 50
+                Option "EmulateWheelTimeout" "1"
+        #       Option "EmulateWheelButton" "9"
+                Option "ZAxisMapping" "4 5"
+        #       Option "XAxisMapping" "6 7"                  # Disable this for vertical-only scrolling.
+        #       Emulate3Buttons refers to the act of pressing buttons A and D
+        #       simultaneously to emulate a middle-click or wheel click.
+                Option "Emulate3Buttons" "true"
+        #       Option "Emulate3Buttons" "true"              # Factory default.
+        EndSection
+      '';
+
+      "X11/xorg.conf.d/20-thinkpad.conf".text = ''
+        Section "InputClass"
+          Identifier  "Trackpoint Wheel Emulation"
+          MatchProduct  "TPPS/2 IBM TrackPoint|DualPoint Stick|Synaptics Inc. Composite TouchPad / TrackPoint|ThinkPad USB Keyboard with TrackPoint|USB Trackpoint pointing device|Composite TouchPad / TrackPoint"
+          MatchDevicePath "/dev/input/event*"
+          Option    "EmulateWheel"    "true"
+          Option    "EmulateWheelButton"  "2"
+          Option    "Emulate3Buttons" "false"
+          Option    "XAxisMapping"    "6 7"
+          Option    "YAxisMapping"    "4 5"
+        EndSection
+      '';
+    };
+
     #variables = {
     # EDITOR = "vim";
     #};
@@ -128,6 +172,7 @@
     autorun = true;
     synaptics.enable = true;
     synaptics.twoFingerScroll = true;
+
     layout = "us(dvp),ru";
     xkbOptions = "grp:caps_toggle";
     videoDrivers = [ "intel" ];
